9ea5fe9d6e6ac4bd15b14dc9cee429c283aacee8
Mark Muya <markmuya@outlook.com>
2014-06-14 14:04:22 +0800

    UPDATE mode

--- js/modalBaptismal.js
+++ js/modalBaptismal.js
@@ -183,8 +183,15 @@ $(document).ready(function(){
 
 					var jsonIdNumber = this.parentNode.getAttribute('data-rf');
 					var editFromDB = 	{
+											inputLastName: json['childLName'+jsonIdNumber],
+											inputFirstName: json['childFName'+jsonIdNumber],
+											inputMiddleName: json['childMName'+jsonIdNumber],
+											bdayMonth: json['bdayMonth'+jsonIdNumber],
+											bdayDay: json['bdayDay'+jsonIdNumber],
+											bdayYear: json['bdayYear'+jsonIdNumber],
 											searchableText: json['text'+jsonIdNumber]
 										};
+										console.log(editFromDB);
 
 					$("#inputLastName").val(editFromDB.inputLastName);
 					$("#inputFirstName").val(editFromDB.inputFirstName);
